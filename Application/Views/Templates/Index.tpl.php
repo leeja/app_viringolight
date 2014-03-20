ca206e1ddd062584944955f61f495fb1a2716966
Jose Lee <jlee.jasoftsolutions@gmail.com>
2014-03-20 18:42:51 -0500

    Colocar Logo en la portada

--- Application/Views/Templates/Index.tpl.php
+++ Application/Views/Templates/Index.tpl.php
@@ -1,7 +1,14 @@
 <html>
 <?PHP include 'Application/Views/Templates/Header.tpl.php';?>
     <body>
-        <h1>Mikhuna</h1> 
+    <center>
+        <br/>
+        <h1>Encuentra lo que estás buscando en...</h1> 
+        <br/>
+        <br/>
+        <br/>
+        <img src="<?PHP echo Class_Config::get('urlApp')?>Public/Images/logotipo.png" height="" width="980" />
+    </center>
 <?PHP include_once 'Application/Views/Templates/Footer.tpl.php';?>        
 </body>
 </html>
